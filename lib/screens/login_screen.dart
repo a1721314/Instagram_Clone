7057220325b395159220a71892bd20ac51103897
t.nagano.g <t.nagano.g@gmail.com>
2022-01-31 21:29:23 +0900

    login_screen編集

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:instagram_clone/utils/colors.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -9,7 +11,22 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Text('From Login Screen'),
-    );
+        body:SafeArea(
+          child:Container(
+            padding: const EdgeInsets.symmetric(horizontal:32),
+            width:double.infinity,
+            child:Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // svg image
+                SvgPicture.asset('assets/ic_instagram.svg', color:primaryColor, height:64,)
+                // text field input for email
+                // text field input for password
+                // button login
+                // Transitioning to signing up
+              ],)
+          )
+        ),
+        );
   }
 }
