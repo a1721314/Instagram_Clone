268014ea83721efe33c812734b390ad3eb621c93
t.nagano.g <t.nagano.g@gmail.com>
2022-02-02 08:11:23 +0900

    login画面作成

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -3,20 +3,24 @@ import 'package:flutter_svg/svg.dart';
 import 'package:instagram_clone/utils/colors.dart';
 import 'package:instagram_clone/widgets/text_field_input.dart';
 
-class LoginScreen extends StatefulWidget {
+class SignupScreen extends StatefulWidget {
   @override
-  _LoginScreenState createState() => _LoginScreenState();
+  _SignupScreenState createState() => _SignupScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _SignupScreenState extends State<SignupScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
 
   @override
   void dispose() {
     super.dispose();
     _emailController.dispose();
     _passwordController.dispose();
+    _bioController.dispose();
+    _usernameController.dispose();
   }
 
   Widget build(BuildContext context) {
@@ -36,10 +40,29 @@ class _LoginScreenState extends State<LoginScreen> {
                     height: 64,
                   ),
                   const SizedBox(height: 64),
+                  // circular widget to accept and show our selected file
+                  Stack(
+                    children: [
+                      CircleAvatar(
+                        radius:64,
+                        //backgrounImage:
+                      )
+                    ],
+                  ),
+
+                  // text field input for username
+                  TextFieldInput(
+                    hintText: 'Enter your username',
+                    textInputType: TextInputType.text,
+                    textEditingController: _usernameController,
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
                   // text field input for email
                   TextFieldInput(
                     hintText: 'Enter your email',
-                    textInputType: TextInputType.emailAddress,
+                    textInputType: TextInputType.text,
                     textEditingController: _emailController,
                   ),
                   const SizedBox(
@@ -55,18 +78,26 @@ class _LoginScreenState extends State<LoginScreen> {
                   const SizedBox(
                     height: 24,
                   ),
+                   TextFieldInput(
+                    hintText: 'Enter your bio',
+                    textInputType: TextInputType.text,
+                    textEditingController: _bioController,
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
                   // button login
                   InkWell(
                     child: Container(
                       child: const Text('Log in'),
                       width: double.infinity,
-                      alignment:Alignment.center,
-                      padding: const EdgeInsets.symmetric(vertical:12),
-                      decoration: const ShapeDecoration(shape:RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(4)
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.symmetric(vertical: 12),
+                      decoration: const ShapeDecoration(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(4)),
                         ),
-                      ),
-                      color: blueColor,
+                        color: blueColor,
                       ),
                     ),
                   ),
@@ -79,22 +110,24 @@ class _LoginScreenState extends State<LoginScreen> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Container(
-                        child: const Text("Don't have an account?"),
-                        padding:const EdgeInsets.symmetric(
-                          vertical: 8,
-                        )
-                      ),
+                          child: const Text("Don't have an account?"),
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 8,
+                          )),
                       GestureDetector(
-                        onTap: (){},
+                        onTap: () {},
                         child: Container(
-                          child: const Text("Sign up.", style: TextStyle(fontWeight: FontWeight.bold,)),
-                          padding:const EdgeInsets.symmetric(
+                          child: const Text("Sign up.",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              )),
+                          padding: const EdgeInsets.symmetric(
                             vertical: 8,
                           ),
                         ),
                       ),
                     ],
-                    ),
+                  ),
                 ],
               ))),
     );
