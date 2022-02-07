8c916430696e8edc0fccfad13896340b1f963db6
t.nagano.g <t.nagano.g@gmail.com>
2022-02-07 21:21:41 +0900

    storage_methods作成

--- lib/resources/storage_methods.dart
+++ lib/resources/storage_methods.dart
@@ -1,5 +1,13 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
-class StorageMethods{
-  
+class StorageMethods {
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // adding image to firebase storage
+  Future<String> uploadImageToStorage(String childName, Uint8List file, bool isPost{
+    
+    _storage.ref().child(childName).child(_auth.currentUser!.uid)
+  }
 }
