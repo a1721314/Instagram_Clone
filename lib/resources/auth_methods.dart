cd95d8bf1b1e9476c89a8f8b86ba38dc3c23f582
t.nagano.g <t.nagano.g@gmail.com>
2022-02-08 22:06:18 +0900

    storage_methods作成

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -5,6 +5,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import 'storage_methods.dart';
+
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
@@ -27,7 +29,13 @@ class AuthMethods {
         //register user
         UserCredential cred = await _auth.createUserWithEmailAndPassword(
             email: email, password: password);
+
+        print(cred.user!.uid);
+
+        String photoUrl = await StorageMethods().uploadImageToStorage('profilePics', file, false);
+
         // add user to our database
+
         await _firestore.collection('users').doc(cred.user!.uid).set({
           'username': username,
           'uid': cred.user!.uid,
@@ -35,6 +43,7 @@ class AuthMethods {
           'bio': bio,
           'followers': [],
           'following': [],
+          'photoUrl': photoUrl,
         });
 
         res = "success";
--- lib/resources/storage_methods.dart
+++ lib/resources/storage_methods.dart
@@ -1,3 +1,6 @@
+import 'dart:typed_data';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
@@ -6,8 +9,15 @@ class StorageMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   // adding image to firebase storage
-  Future<String> uploadImageToStorage(String childName, Uint8List file, bool isPost{
-    
-    _storage.ref().child(childName).child(_auth.currentUser!.uid)
+  Future<String> uploadImageToStorage(
+      String childName, Uint8List file, bool isPost) async {
+    Reference ref =
+        _storage.ref().child(childName).child(_auth.currentUser!.uid);
+
+    UploadTask uploadTask = ref.putData(file);
+
+    TaskSnapshot snap = await uploadTask;
+    String downloadUrl = await snap.ref.getDownloadURL();
+    return downloadUrl;
   }
 }
